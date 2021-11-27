f9458efdb8d9ba4fc56c85b28405cf9ef5e9daf6
Andrew Miroiu <andrew.miroiu@gmail.com>
2021-11-27 18:36:40 +0200

    added cookie clicker v1.0 game source code

---
+++ src/Cookie Clicker/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.costTimer = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Image = global::WinFormsApp3.Properties.Resources.cookie_removebg_preview;
+            this.button1.Location = new System.Drawing.Point(302, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 200);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(360, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cookies: 0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(615, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Increase cookies per click by 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(671, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "COST: 100";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cookies per click:  1";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(615, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Increase cookies per click by 5";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(671, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "COST: 250";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 426);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 15);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(302, 426);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(177, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Copyright Andrei Miroiu @ 2021";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(335, 67);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 15);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Cookies per second: 0";
+            // 
+            // costTimer
+            // 
+            this.costTimer.AutoSize = true;
+            this.costTimer.Location = new System.Drawing.Point(80, 30);
+            this.costTimer.Name = "costTimer";
+            this.costTimer.Size = new System.Drawing.Size(69, 15);
+            this.costTimer.TabIndex = 10;
+            this.costTimer.Text = "COST : 2500";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(214, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Increase your cookies per second by 1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.costTimer);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Cookie Clicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label costTimer;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ src/Cookie Clicker/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+
+        private int counter = 0;
+        private int costButton2 = 100, costButton3 = 250, costTimer1 = 2500;
+        private int counterIncreaser = 1;
+        private int counterIncreaserForSeconds = 0;
+        private bool verifyPressedButton4 = false;
+        //private System.Timers.Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+
+            //setupTimer();
+        }
+
+        /*private void setupTimer()
+        {
+            timer = new System.Timers.Timer();
+            timer.Enabled = true;
+            timer.Interval = 1000;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(onTimerElapsed);
+            timer.Start();
+        }
+
+        private void onTimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("onTimerElapsed");
+        }*/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            increaseCounter();
+            this.label1.Text = string.Format("Cookies: {0}", counter);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (counter >= costButton2)
+            {
+                increasingCounterIncreaser(1);
+                counter = counter - costButton2;
+                costButton2 = costButton2 + 100;
+                this.label2.Text = "COST:" + costButton2;
+                this.label3.Text = "Cookies per click: " + counterIncreaser;
+                this.label1.Text = string.Format("Cookies: {0}", counter);
+            }
+
+            else
+            {
+                notEnoughToBuy();
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (counter >= costButton3)
+            {
+                increasingCounterIncreaser(5);
+                counter = counter - costButton3;
+                costButton3 = costButton3 + 250;
+                this.label4.Text = "COST: " + costButton3;
+                this.label3.Text = "Cookies per click: " + counterIncreaser;
+                this.label1.Text = string.Format("Cookies: {0}", counter);
+            }
+            else
+            {
+                notEnoughToBuy();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (counter >= costTimer1)
+            {
+                counterIncreaserForSeconds = counterIncreaserForSeconds + 1;
+                counter = counter - costTimer1;
+                costTimer1 = costTimer1 + 2500;
+                this.costTimer.Text = "COST: " + costTimer1;
+                this.label7.Text = "Cookies per second: " + counterIncreaserForSeconds;
+                verifyPressedButton4 = true;
+            }
+            else
+            {
+                notEnoughToBuy();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (verifyPressedButton4)
+            {
+                this.label1.Text = string.Format("Cookies: {0}", counter);
+                counter = counter + counterIncreaserForSeconds;
+            }
+        }
+
+        #region boilerplatecode
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        private void notEnoughToBuy()
+        {
+            MessageBox.Show("You don't have enough cookies", "YOU'RE BANKRUPT", MessageBoxButtons.OKCancel);
+        }
+
+        private void increaseCounter()
+        {
+            counter = counter + counterIncreaser;
+        }
+
+        private void increasingCounterIncreaser(int n)
+        {
+            counterIncreaser = counterIncreaser + n;
+        }
+    }
+}
